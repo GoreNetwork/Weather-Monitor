839af14a6f0bcb69be914ff9b660239c5584302c
daniel himes <dhimes@gmail.com>
2021-10-16 15:05:29 -0400

    Basically working

--- send_text_message.py
+++ send_text_message.py
@@ -7,12 +7,14 @@ from creds import auth_token, account_sid
 # To set up environmental variables, see http://twil.io/secure
 
 def send_txt_message(message):
-    phone_numbers = ["+14348412662"]
+    # Numbers you want to send text messages to
+    phone_numbers = ["+14345551212"]
     for phone_number in phone_numbers:
         client = Client(account_sid, auth_token)
         client.api.account.messages.create(
             to=phone_number,
-            #Has to be a phonenumber you own in twilio
-            from_="+19842082527",
+            # Has to be a phonenumber you own in twilio
+            # This is the number the text will be from
+            from_="+19845551212",
             body=message)
 
